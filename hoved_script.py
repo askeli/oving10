3e9d7030d82f7581eb59d4ca8d8e2835bd116560
askeli <christoffer.askeli@gmail.com>
2022-11-15 18:00:28 +0100

    progressbar

--- hoved_script.py
+++ hoved_script.py
@@ -184,31 +184,20 @@ def ny_kategori_til_avtale():
 
 #Progressbar
 def progress_bar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iterable    - Required  : iterable object (Iterable)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
     total = len(iterable)
-    # Progress Bar Printing Function
+    # Progress bar printfunksjon
     def printProgressBar (iteration):
         percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
         filledLength = int(length * iteration // total)
         bar = fill * filledLength + '-' * (length - filledLength)
         print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Initial Call
+    # Init
     printProgressBar(0)
-    # Update Progress Bar
+    # Oppdaterer progress bar
     for i, item in enumerate(iterable):
         yield item
         printProgressBar(i + 1)
-    # Print New Line on Complete
+    # Skriv ny linje når ferdig
     print()
 
 
